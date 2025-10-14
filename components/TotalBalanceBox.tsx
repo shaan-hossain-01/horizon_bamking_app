11932b6c53692300fe3c3ef88b2c25ed06b5c410
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-10-14 17:31:59 +0600

    update: enhance TotalBalanceBox component implementation

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,11 +1,24 @@
+import { formatAmount } from "@/lib/utils";
+
 const TotalBalanceBox = ({
-  accounts,
+  accounts = [],
   totalBanks,
   totalCurrentBalance,
 }: TotalBalanceBoxProps) => {
-  return <div>
-    
-  </div>;
+  return (
+    <section className="total-balance">
+      <div className="total-balance-box">{/* DoughNut Chart */}</div>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <p className="total-balance-amount flex-center gap-2">
+            {formatAmount(totalCurrentBalance)}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TotalBalanceBox;
