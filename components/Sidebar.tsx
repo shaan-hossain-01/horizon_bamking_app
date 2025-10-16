81eae76397e599a6cf0b7421cd17d9381429e2c1
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-10-16 16:13:45 +0600

    feat: add Sidebar component and update root layout

---
+++ components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            alt="Horizon"
+            width={34}
+            height={34}
+            className="size-[24px] max-xl:size-12"
+          />
+        </Link>
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
